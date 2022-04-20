96de5e4236089ed65718197ef5d783bf71772ccd
capricornswap <capricornswap@gmail.com>
2022-04-20 16:39:51 +0800

    fix bug

--- contracts/CapricornToken.sol
+++ contracts/CapricornToken.sol
@@ -14,12 +14,12 @@ contract CapricornToken is CRC20('CapricornSwap Token', 'CPCT') {
     constructor() public {
         uint chainId;
         assembly {
-            chainId := chainid
+            chainId := chainid()
         }
         DOMAIN_SEPARATOR = keccak256(
             abi.encode(
                 keccak256('EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)'),
-                keccak256(bytes(name)),
+                keccak256(bytes(name())),
                 keccak256(bytes('1')),
                 chainId,
                 address(this)
