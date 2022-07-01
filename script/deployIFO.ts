b1066ce7d13832b0c4933977a332b622ee4898e5
capricornswap <capricornswap@gmail.com>
2022-07-01 12:19:51 +0800

    update deploy script

--- script/deployIFO.ts
+++ script/deployIFO.ts
@@ -18,6 +18,16 @@ async function main() {
   console.log("Deploying contracts with the account:", deployer.address);
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
+
+/* cube-testnet internal
+  const masterchef = '0x4b3f5a330A64D3CFAa33ba6E2D2936a195904f38';
+  const swapFactor = '0xc524aed399c4dc98c42af8fab58f440ff5cd36f0';
+  const USDT = '0x9bd522cc85bd1bd6d069d5e273e46ccfee905493';
+
+  const wcube = '0xB9164670A2F388D835B868b3D0D441fa1bE5bb00';
+  const swapRouter = '0xc82edd7f219d0771f4bc45353d67fc4c8d34c1ee';
+//*/
+
 /* cube-testnet
   const masterchef = '0x6273638e3Be5770851E23bfcE27d69592BEDCd2c';
   const swapFactor = '0x7a1eba426aa389aac9b410cdfe3cef5d344e043f';
@@ -34,6 +44,7 @@ async function main() {
 
   const wcube = '0x9d3f61338d6eb394e378d28c1fd17d5909ac6591';
   const swapRouter = '0x34c385dd9015d663830a37CD2E75818fda6C605f';
+  const feeToAddr = '0x37e45820279efefb805f71707883c438bed9e0d4'
 //*/
 
   const Validator= await ethers.getContractFactory("Validator");
@@ -49,12 +60,17 @@ async function main() {
   console.log("deploy ifoFactory",ifoFactory.address);
 
   // setMinTime
-  const setMinTime = await ifoFactory.setMinTime(0);
+  const setMinTime = await ifoFactory.setMinTime(600);
   await setMinTime.wait();
   console.log("setMinTime",await ifoFactory.MIN_TIME());
 
+  // setMinPeriod
+  const setMinPeriod = await ifoFactory.setMinPeriod(30);
+  await setMinPeriod.wait();
+  console.log("setMinPeriod",await ifoFactory.MIN_PERIOD());
+
   // setFeeTo
-  const setFeeTo = await ifoFactory.setFeeTo(deployer.address);
+  const setFeeTo = await ifoFactory.setFeeTo(feeToAddr);
   await setFeeTo.wait();
   console.log("setFeeTo",await ifoFactory.feeTo());
 
